c5886f7d9872614264c99159a2b86580c96a99f5
esteban alvarado <estalvgs1999@gmail.com>
2021-03-03 20:45:24 -0600

    Archivos  del código fuente de la librería creados

---
+++ FunTrans/funtras.h
@@ -0,0 +1,11 @@
+#ifndef _FUNTRAS_H
+#define _FUNTRAS_H
+
+double div_t(double x);
+double sin_t(double x);
+double tan_t(double x);
+double log_t(double x, int a);
+double sinh_t(double x);
+// TODO: continuar declarando las funciones
+
+#endif
