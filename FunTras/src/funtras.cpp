037e951dd2d802acc7524b68348abf7f7c0a55a3
esteban alvarado <estalvgs1999@gmail.com>
2021-04-05 11:27:05 -0600

    [fix] Error in div_t fixed

--- FunTras/src/funtras.cpp
+++ FunTras/src/funtras.cpp
@@ -37,7 +37,7 @@ long double funtras::div_t(double x){
     double x_sgt = x * (2 - a*x); // Calcular el valor siguiente
     int k = 1;
 
-    while (abs(x_sgt - x)/x_sgt > tolerancia || k < max_iteraciones){
+    while (abs(x_sgt - x)/x_sgt > tolerancia && k < max_iteraciones){
         double tmp = x_sgt;
         x_sgt = x * (2 - a*x); 
         x = x_sgt;
